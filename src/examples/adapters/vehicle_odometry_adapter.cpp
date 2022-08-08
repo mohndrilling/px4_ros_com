6285bd8ed04304ee1ed1985248d6f0cd12da61ab
auv <florian.pix.97@outlook.com>
2022-08-08 14:31:48 +0200

    replaced estimator_odometry_adapter with vehicle_odometry_adapter

--- src/examples/adapters/vehicle_odometry_adapter.cpp
+++ src/examples/adapters/vehicle_odometry_adapter.cpp
@@ -1,13 +1,13 @@
 /**
- * @brief estimator odometry uORB-ROS2 topic listener and odometry ROS2 topic advertiser
- * @file estimator_odometry_adapter.cpp
+ * @brief vehicle odometry uORB-ROS2 topic listener and odometry ROS2 topic advertiser
+ * @file vehicle_odometry_adapter.cpp
  * @addtogroup examples
  * @author Florian Pix <florian.pix.97@outlook.com>
  */
 
 #include <chrono>
 #include <rclcpp/rclcpp.hpp>
-#include <px4_msgs/msg/estimator_odometry.hpp>
+#include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Core>
@@ -16,54 +16,59 @@ using std::placeholders::_1;
 using namespace std::chrono_literals;
 using Eigen::MatrixXd;
 
-class EstimatorOdometryAdapter : public rclcpp::Node
+class VehicleOdometryAdapter : public rclcpp::Node
 {
 public:
+    bool frame_info_enable = true;
+
     MatrixXd pose_covariance = MatrixXd(6,6);
     MatrixXd twist_covariance = MatrixXd(6,6);
 
-	EstimatorOdometryAdapter() : Node("estimator_odometry_adapter") {
+	VehicleOdometryAdapter() : Node("vehicle_odometry_adapter") {
         // create publisher for odometry ROS2 topic
 	    publisher_odometry_ = this->create_publisher<nav_msgs::msg::Odometry>("/model/auv/odometry", 10);
 		
         // create subscriber for estimator odometry uORB-ROS2 topic
-        subscription_ = this->create_subscription<px4_msgs::msg::EstimatorOdometry>(
-            "fmu/estimator_odometry/out", 
+        subscription_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>(
+            "fmu/vehicle_odometry/out", 
             10, 
-            std::bind(&EstimatorOdometryAdapter::odometry_callback, this, _1)
+            std::bind(&VehicleOdometryAdapter::odometry_callback, this, _1)
         );
 	}
 
-    void odometry_callback(const px4_msgs::msg::EstimatorOdometry::SharedPtr msg)
+    void odometry_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg)
     {
-        switch (msg->local_frame) {
-            case 0:
-                RCLCPP_INFO(this->get_logger(), "local frame: NED earth-fixed frame");
-                break;
-            case 1:
-                RCLCPP_INFO(this->get_logger(), "local frame: FRD earth-fixed frame, arbitrary heading reference");
-                break;
-            case 2:
-                RCLCPP_INFO(this->get_logger(), "local frame: not aligned with the std frames of reference");
-                break;
-            case 3:
-                RCLCPP_INFO(this->get_logger(), "local frame: using FRD body-fixed frame");
-                break;
+        if (frame_info_enable){
+            switch (msg->local_frame) {
+                case 0:
+                    RCLCPP_INFO(this->get_logger(), "local frame: NED earth-fixed frame");
+                    break;
+                case 1:
+                    RCLCPP_INFO(this->get_logger(), "local frame: FRD earth-fixed frame, arbitrary heading reference");
+                    break;
+                case 2:
+                    RCLCPP_INFO(this->get_logger(), "local frame: not aligned with the std frames of reference");
+                    break;
+                case 3:
+                    RCLCPP_INFO(this->get_logger(), "local frame: using FRD body-fixed frame");
+                    break;
+            }
+            switch (msg->velocity_frame) {
+                case 0:
+                    RCLCPP_INFO(this->get_logger(), "velocity frame: NED earth-fixed frame");
+                    break;
+                case 1:
+                    RCLCPP_INFO(this->get_logger(), "velocity frame: FRD earth-fixed frame, arbitrary heading reference");
+                    break;
+                case 2:
+                    RCLCPP_INFO(this->get_logger(), "velocity frame: not aligned with the std frames of reference");
+                    break;
+                case 3:
+                    RCLCPP_INFO(this->get_logger(), "velocity frame: using FRD body-fixed frame");
+                    break;
+            }
         }
-        switch (msg->velocity_frame) {
-            case 0:
-                RCLCPP_INFO(this->get_logger(), "velocity frame: NED earth-fixed frame");
-                break;
-            case 1:
-                RCLCPP_INFO(this->get_logger(), "velocity frame: FRD earth-fixed frame, arbitrary heading reference");
-                break;
-            case 2:
-                RCLCPP_INFO(this->get_logger(), "velocity frame: not aligned with the std frames of reference");
-                break;
-            case 3:
-                RCLCPP_INFO(this->get_logger(), "velocity frame: using FRD body-fixed frame");
-                break;
-        }
+
         auto odometry = nav_msgs::msg::Odometry();
         odometry.pose.pose.position.x = msg->x;
         odometry.pose.pose.position.y = msg->y;
@@ -105,20 +110,55 @@ public:
         odometry.header.stamp.sec = this->get_clock()->now().seconds();
 
         this->publisher_odometry_->publish(odometry);
+
+        RCLCPP_INFO(this->get_logger(), "publishing:");
+        RCLCPP_INFO
+        (
+            this->get_logger(), 
+            "position: x: %f, y: %f, z: %f",  
+            odometry.pose.pose.position.x,
+            odometry.pose.pose.position.y,
+            odometry.pose.pose.position.z
+        );
+        RCLCPP_INFO
+        (
+            this->get_logger(), 
+            "orientation: q1: %f, q2: %f, q3: %f, q4: %f",
+            odometry.pose.pose.orientation.x,
+            odometry.pose.pose.orientation.y,
+            odometry.pose.pose.orientation.z,
+            odometry.pose.pose.orientation.w
+        );
+        RCLCPP_INFO
+        (
+            this->get_logger(), 
+            "linear velocity: x: %f, y: %f, z: %f",
+            odometry.twist.twist.linear.x,
+            odometry.twist.twist.linear.y,
+            odometry.twist.twist.linear.z
+        );
+        RCLCPP_INFO
+        (
+            this->get_logger(), 
+            "angular velocity: roll: %f, pitch: %f, yaw: %f",
+            odometry.twist.twist.angular.x,
+            odometry.twist.twist.angular.y,
+            odometry.twist.twist.angular.z
+        );
     }
 
 private:
-    rclcpp::Subscription<px4_msgs::msg::EstimatorOdometry>::SharedPtr subscription_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr subscription_;
 
 	rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_odometry_;
 };
 
 int main(int argc, char *argv[])
 {
-	std::cout << "estimator_odometry_adapter node..." << std::endl;
+	std::cout << "vehicle_odometry_adapter node..." << std::endl;
 	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<EstimatorOdometryAdapter>());
+	rclcpp::spin(std::make_shared<VehicleOdometryAdapter>());
 
 	rclcpp::shutdown();
 	return 0;
